7b06349a30fc7c0dd04e9658836b892f4b0680bd
Katie McGowan <katiemcgowan@Matts-MacBook-Air.local>
2022-05-19 14:12:30 -0700

    Updates player and computer card HTML whena  card is played, makes input field dynamic

--- pokemon.js
+++ pokemon.js
@@ -136,97 +136,123 @@
 // Do other cool stuff!
 // Look at the Pokemon cards and add additional Pokemon data to the card objects. Have your cards fight and win the "real" Pokemon card game way.
 
-
 const cards = [
-    {
-      name: "Bulbasaur",
-      damage: 60
-    }, {
-      name: "Caterpie",
-      damage: 40
-    }, {
-      name: "Charmander",
-      damage: 60
-    }, {
-      name: "Clefairy",
-      damage: 50
-    }, {
-      name: "Jigglypuff",
-      damage: 60
-    }, {
-      name: "Mankey",
-      damage: 30
-    }, {
-      name: "Meowth",
-      damage: 60
-    }, {
-      name: "Nidoran - female",
-      damage: 60
-    }, {
-      name: "Nidoran - male",
-      damage: 50
-    }, {
-      name: "Oddish",
-      damage: 40
-    }, {
-      name: "Pidgey",
-      damage: 50
-    }, {
-      name: "Pikachu",
-      damage: 50
-    }, {
-      name: "Poliwag",
-      damage: 50
-    }, {
-      name: "Psyduck",
-      damage: 60
-    }, {
-      name: "Rattata",
-      damage: 30
-    }, {
-      name: "Squirtle",
-      damage: 60
-    }, {
-      name: "Vulpix",
-      damage: 50
-    }, {
-      name: "Weedle", 
-      damage: 40
-    }
-  ]
+  {
+    name: "Bulbasaur",
+    damage: 60
+  }, {
+    name: "Caterpie",
+    damage: 40
+  }, {
+    name: "Charmander",
+    damage: 60
+  }, {
+    name: "Clefairy",
+    damage: 50
+  }, {
+    name: "Jigglypuff",
+    damage: 60
+  }, {
+    name: "Mankey",
+    damage: 30
+  }, {
+    name: "Meowth",
+    damage: 60
+  }, {
+    name: "Nidoran - female",
+    damage: 60
+  }, {
+    name: "Nidoran - male",
+    damage: 50
+  }, {
+    name: "Oddish",
+    damage: 40
+  }, {
+    name: "Pidgey",
+    damage: 50
+  }, {
+    name: "Pikachu",
+    damage: 50
+  }, {
+    name: "Poliwag",
+    damage: 50
+  }, {
+    name: "Psyduck",
+    damage: 60
+  }, {
+    name: "Rattata",
+    damage: 30
+  }, {
+    name: "Squirtle",
+    damage: 60
+  }, {
+    name: "Vulpix",
+    damage: 50
+  }, {
+    name: "Weedle", 
+    damage: 40
+  }
+]
 
+document.getElementById('quantity').style.visibility = 'hidden';
+document.getElementById('playhand').style.visibility = 'hidden';
+document.getElementById('validity').style.visibility = 'hidden';
+//Array for comparing played cards
 const playedCards = [];
 
+//Player object that stores score, cards dealt to them, previously played cards, and has method to play card
 const player = {
   score: 0,
   currentHand: [],
   previouslyPlayed: [],
+  //Play card method, takes num as an argument (grabbed from number input on page)
   playCard (num) {
     console.log("Player's turn");
+    //Declares index variable because number submitted on page is 1-3, whereas the arrays being modified start at index 0
     let index = num - 1; 
     let playedCard = this.currentHand[index];
     playedCards.push(playedCard);
+    console.log(playedCard);
     this.previouslyPlayed.push(playedCard);
     this.currentHand.splice(index,1)
+    console.log(this.currentHand)
     //Updates played card field
+    //Creates new HTML p-tag element
     const playerPlayedPara = document.createElement('p');
+    //Gives new p-tag element an ID to call upon later for editing. pp is an acronym for player played, concatenating with roundCounter makes ID unique every round
+    playerPlayedPara.id = "pp" + roundCounter;
+    //Creates text node that is appended to the new p-tag element
     const playerPlayedNode = document.createTextNode(playedCard.name + ": " + playedCard.damage);
     playerPlayedPara.appendChild(playerPlayedNode);
+    //Apphends playerplayed HTML element to include new p-tag and text nodes
     const playerElement = document.getElementById('playerplayed');
     playerElement.appendChild(playerPlayedPara);
-    //Removes played Pokemon from playercard
+    //Removes played Pokemon from playercard by calling upon HTML ID, storing in a variable, and removing it
     let playedEntry = document.getElementById("p" + index)
+    //**********HAVE TO MAKE THIS DYNAMIC**************/
     playedEntry.remove()
+    //Changes the p id's and updates the numbers next to Pokemon
+    const playerContainer = document.querySelector('#playercard')
+    let pArray = playerContainer.querySelectorAll('p');
+    for (let i = 0; i < pArray.length; i++) {
+      for (let element of pArray) {
+        element.id = "p" + element;
+      } 
+      pArray[i].innerHTML = (i + 1) + ": " + this.currentHand[i].name + ": " + this.currentHand[i].damage;
+    }
+    //Updates number field to reflect player hand's length
+    document.getElementById('quantity').max = player.currentHand.length;
+    console.log(document.getElementById('quantity').max = player.currentHand.length);
   }
-}
-  
-//stored HTML element in p0, p2, p3. Call on index to remove element 
+}  
 
+//Computer object that stores score, cards dealt to them, previously played cards, and has method to play card
 const computer = {
   score: 0,
   currentHand: [],
   previouslyPlayed: [],
   computerSelection: 0,
+  //Computer-specific method that randomly selects an index from currentHand array; returns a value to be plugged into playCard method in round function
   autoSelect() {
     const min = Math.ceil(0);
     const max = Math.floor(this.currentHand.length - 1);
@@ -237,39 +263,56 @@ const computer = {
     let index = num; 
     let playedCard = this.currentHand[index];
     playedCards.push(playedCard);
+    console.log(playedCard);
     this.computerSelection = 0;
     this.previouslyPlayed.push(playedCard);
     this.currentHand.splice(index,1)
+    console.log(this.currentHand)
     //Updates played card field
-    const computerplayedpara = document.createElement('p');
-    const computerplayednode = document.createTextNode(playedCard.name + ": " + playedCard.damage);
-    computerplayedpara.appendChild(computerplayednode);
-    const computerelement = document.getElementById('computerplayed');
-    computerelement.appendChild(computerplayedpara);
-    //Removes played Pokemon from playercard
+    //Creates new HTML p-tag element
+    const computerPlayedPara = document.createElement('p');
+    //Gives new p-tag element an ID to call upon later for editing. cp is an acronym for computer played, concatenating with roundCounter makes ID unique every round
+    computerPlayedPara.id = "cp" + roundCounter;
+    //Creates text node that is appended to the new p-tag element
+    const computerPlayedNode = document.createTextNode(playedCard.name + ": " + playedCard.damage);
+    computerPlayedPara.appendChild(computerPlayedNode);
+     //Apphends playerplayed HTML element to include new p-tag and text nodes
+    const computerElement = document.getElementById('computerplayed');
+    computerElement.appendChild(computerPlayedPara);
+    //Removes played Pokemon from computercard by calling upon HTML ID, storing in a variable, and removing it
     let playedEntry = document.getElementById("c" + index)
     playedEntry.remove()
+    // Manipulating computerside hand
+    const computerContainer = document.querySelector('#computercard')
+    const cArray = computerContainer.querySelectorAll('p');
+    for (let i = 0; i < cArray.length; i++) {
+      for (let element of cArray) {
+        element.id = "c" + element;
+      }
+      cArray[i].innerHTML = (i + 1) + ": " + this.currentHand[i].name + ": " + this.currentHand[i].damage;
     }
-  }
+  }    
+}
 
-
-//Creating p tag for playercard
-//Creating a text node 
-//Apphending p tag with text node
-//Apphending playercard with apphended p tag
 
 const dealHand = () => {
+  //For loop that runs three times to deal out three cards to player, utilizes math.Random to ensure random dealing
   for (let i = 0; i < 3; i++) {  
     const min = Math.ceil(0);
     const max = Math.floor(cards.length - 1);
     let handIndex = Math.floor(Math.random() * (max - min + 1) + min);
+    //Moves Pokemon objects from cards array to individual object arrays
     player.currentHand.push(cards[handIndex]);
+    //Removes dealt hand from cards array to ensure there are no duplicates
     cards.splice(handIndex,1);
+    //Creates new p-tag HTML element and gives it an ID concatenated with i value to make every HTML element unique
     const playerPara = document.createElement('p');
     playerPara.id = "p" + i;
+    //At the top of the round, creates text node and populates with random cards
     if (roundCounter == 0) {
       const playerNode = document.createTextNode((i+1) + ": " + player.currentHand[i].name + ": " + player.currentHand[i].damage);
       playerPara.appendChild(playerNode);
+      //Apphends card info to playercard HTML element
       const playerElement = document.getElementById('playercard');
       playerElement.appendChild(playerPara);
     } else if (roundCounter == 2) {
@@ -279,14 +322,19 @@ const dealHand = () => {
       playerElement.appendChild(playerPara);
     } else console.log("Error");
   }
+  //For loop that runs three times to deal out three cards to computer, utilizes math.Random to ensure random dealing
   for (let i = 0; i < 3; i++) {  
     const min = Math.ceil(0);
     const max = Math.floor(cards.length - 1);
     let handIndex = Math.floor(Math.random() * (max - min + 1) + min);
+    //Moves Pokemon objects from cards array to individual object arrays
     computer.currentHand.push(cards[handIndex]);
+    //Removes dealt hand from cards array to ensure there are no duplicates
     cards.splice(handIndex,1);
+    //Creates new p-tag HTML element and gives it an ID concatenated with i value to make every HTML element unique
     const computerPara = document.createElement('p');
     computerPara.id = "c" + i;
+    //At the top of the round, creates text node and populates with random cards
     if (roundCounter == 0) {
       const computerNode = document.createTextNode((i+1) + ": " + computer.currentHand[i].name + ": " + computer.currentHand[i].damage);
       computerPara.appendChild(computerNode);
@@ -299,25 +347,13 @@ const dealHand = () => {
       computerElement.appendChild(computerPara);
     } else console.log("Error");
   }
-
   console.log(player.currentHand);
   console.log(computer.currentHand);
+  if (roundCounter == 0) {
   roundCounter++;
+  };
+  document.getElementById('deal').style.visibility = 'hidden';
 }
-
-/* <div id="div1">
-  <p id="p1">This is a paragraph.</p>
-  <p id="p2">This is another paragraph.</p>
-</div>
-
-<script>
-const para = document.createElement("p");
-const node = document.createTextNode("This is new.");
-para.appendChild(node);
-
-const element = document.getElementById("div1");
-element.appendChild(para);
-</script> */
 
 let roundCounter = 0;
 
@@ -344,15 +380,20 @@ const round = (num) => {
     } else {
       console.log("Tie!")
     }
-    roundCounter++;
     playedCards.splice(0,2); 
   } else {
     console.log("Game over!")
-      if (player.score > computer.score) {
-        console.log("Player wins!")
-      } else {
-        console.log("Computer wins!");
-      }
+    if (player.score > computer.score) {
+      console.log("Player wins!")
+    } else {
+      console.log("Computer wins!");
+    }
+  }
+  roundCounter++;
+  console.log("Round: " + roundCounter)
+  document.getElementById("roundcounter").innerHTML = "Round: " + roundCounter;
+  if (player.currentHand.length == 0) {
+    document.getElementById('deal').style.visibility = 'visibile';
   }
 };
 
@@ -360,4 +401,21 @@ const dealButton = document.getElementById("deal");
 dealButton.addEventListener("click", () => dealHand());
 dealButton.addEventListener("click", function(){
   document.getElementById("roundcounter").innerHTML = "Round: " + roundCounter;
+  document.getElementById('quantity').style.visibility = 'visible';
+  document.getElementById('playhand').style.visibility = 'visible';
+  document.getElementById('validity').style.visibility = 'visible';
 });
+
+// document.getElementById("cp" + roundCounter).style.textDecoration = "line-through";
+
+//List of issues: 
+//HTML for Pokemon in the playercard and computercard (numbers), are not responsive. 
+//if (p + number) != num (selected by user) 
+//for loop to change p to 0,1, etc. 
+//change id to array index + 1
+
+
+//Refactoring: 
+//Take out the number system, make new textNodes be clickable and correlated with the indexes of playerHand/conputerHand
+//Created p-id's for playerhand = p0, p1, p2
+//p onclick execute round(num);
